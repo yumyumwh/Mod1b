58bbcb44ef8d2d49899c7603d61d5e9ab2fb08a3
yumyumwh <130779608+yumyumwh@users.noreply.github.com>
2026-09-11 14:44:45 +0200

    Add files via upload

---
+++ Project.r
@@ -0,0 +1,21 @@
+library(ggplot2)
+
+
+# A <-> B
+
+
+#parameters
+
+para_a = 1
+para_b = 1
+r_a = 1
+r_b = 1
+
+#equations
+
+#a[n+1] = r_a * a[n] * (1 - a[n] - para_b * b[n])
+#b[n+1] = r_b * b[n] * (1 - b[n] - para_a * a[n])
+
+#consider only equilibrium state
+
+a = 1 - para_b - 1/r_a
